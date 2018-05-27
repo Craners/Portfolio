2d5bc612787ae6b648860fd1e567dbb04e3de17e
Amir <kmuc72@gmail.com>
2018-05-27 15:03:54 +0200

    added the image component and the divider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ImageComponent } from './image/image.component';
+<<<<<<< Updated upstream
 import { MaterialModule } from "./material/material.module";
+=======
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatGridListModule,MatGridTile} from '@angular/material/grid-list';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -11,7 +17,14 @@ import { MaterialModule } from "./material/material.module";
     ImageComponent
   ],
   imports: [
+<<<<<<< Updated upstream
     BrowserModule
+=======
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatGridListModule
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
