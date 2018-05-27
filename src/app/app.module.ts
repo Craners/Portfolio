19a3bc072cd0c16522edfc08905106d3089f419d
Amir <kmuc72@gmail.com>
2018-05-27 12:40:22 +0200

    angular module was added. image component were added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { ImageComponent } from './image/image.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/image/image.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageComponent } from './image.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ImageComponent]
+})
+export class ImageModule { }
