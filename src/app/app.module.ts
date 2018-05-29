1814857a7da48d535f818ad32dd8952b5c3dba5e
Samin137 <Samin-Asnaashari@users.noreply.github.com>
2018-05-30 00:09:29 +0200

    Added social-links component. Added SVG icons in assets for social links icons. the research was done for choosing the possible design framework. Among the results, we can consider Bootstrap, Semantic UI and Angular material (Design Material). Font-awesome was also considered, unfortunately, its integration is not yet ready.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ImageComponent } from './image/image.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDividerModule } from '@angular/material/divider';
 import {MatGridListModule,MatGridTile} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageComponent
+    ImageComponent,
+    SocialLinksComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatDividerModule,
-    MatGridListModule
+    MatGridListModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
