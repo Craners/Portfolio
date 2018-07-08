f806e7cfc314cb43acad24ba5d2eeb2a6d66261d
amir <kmuc72@gmail.com>
2018-07-08 17:25:34 +0200

    travis build please

--- buildScriptTravis.sh
+++ buildScriptTravis.sh
@@ -1,4 +1,3 @@
 echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | sudo tee -a /etc/apt/sources.list
 wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
 sudo apt-get update && sudo apt-get install git-secret
-
