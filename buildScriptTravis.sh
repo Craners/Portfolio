d6557bfac42405ea39b12528bcb3e75bf9e44540
amir <kmuc72@gmail.com>
2018-07-08 17:40:33 +0200

    modified bash script

--- buildScriptTravis.sh
+++ buildScriptTravis.sh
@@ -1,4 +1,4 @@
 echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | sudo tee -a /etc/apt/sources.list
 wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
-sudo apt-get update && sudo apt-get install git-secret
+apt-get update && apt-get install git-secret
 git secret reveal 
